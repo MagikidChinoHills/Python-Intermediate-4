b30ebbc0094eee5253307608c9ab979ee5d10d2d
CyberRaptor21 <johnsk7789@gmail.com>
2025-02-28 13:26:31 -0800

    Create Kylar_Lesson1.py

---
+++ Homework Submission/Kylar_Lesson1.py
@@ -0,0 +1,58 @@
+# Problem 1
+import turtle
+
+t = turtle.Turtle
+for _ in range(3):
+    t.forward(100)
+    t.left(144)
+# Problem 2
+import turtle
+
+def draw_polygon(side, length):
+t = turtle.Turtle         
+    t.forward(80)
+    t.left(145)
+    t.forward(80)
+    t.left(146)
+    t.forward(80)
+    t.left(147)
+    t.forward(80)
+    t.left(148)
+    t.forward(80)
+    t.left(149)
+    t.forward(80)
+    t.left(150)
+    t.forward(80)
+    t.left(151)
+    t.forward(80)
+    t.left(152)
+turtle.done()
+# Problem 3
+import turtle
+
+t = turtle.Turtle
+t.fill("blue")
+for _ in range(2)
+    t.forward(150)
+    t.left(75)
+# Problem 4
+import turtle
+
+t = turtle.Turtle
+t.fill("red")
+t.forward(100)
+t.left(144)
+t.forward(200)
+t.left(145)
+t.forward(200)
+t.left(146)
+t.forward(200)
+t.left(147)
+t.forward(200)
+t.left(148)
+# Problem 5
+import turtle
+def circle(side, length):
+    t = turtle.Turtle
+    t.side(100, 4)
+# I'm pretty bad at python
