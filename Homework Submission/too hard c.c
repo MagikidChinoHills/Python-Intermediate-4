e5c3f28c1948413cd52d87c059ad5750dbceca2f
charleschenyifeng <charleschen140313@gmail.com>
2025-03-22 17:26:05 -0700

    Create too hard c.c

---
+++ Homework Submission/too hard c.c
@@ -0,0 +1,10 @@
+import turtle
+
+tool = turtle.Turtle()
+tool.fillcolo('red')
+  tool.begine_fill()
+  for _ in range(2):
+    tool.forward(80)
+    tool.left(360/6)
+    tool.end_fill()
+turtle.done()
