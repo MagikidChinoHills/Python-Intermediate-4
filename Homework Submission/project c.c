d1368455c71e3ea2e767c0a3e48bf15d4ca794cf
charleschenyifeng <charleschen140313@gmail.com>
2025-04-13 12:16:13 -0700

    Update project c.c

--- Homework Submission/project c.c
+++ Homework Submission/project c.c
@@ -1,5 +1,6 @@
 1
 import turtle
+import random
 
 tool = turtle.Turtle()
 tool.begin_fill()
@@ -58,6 +59,43 @@ for _ in range(2):
 
 
 tool.end_fill()
+
+tool.penup
+tool.goto(66, 75)
+tool.pendown
+
+
+def draw_tree(branch_length):
+    if branch_length > 5: 
+        tool.forward(branch_length) 
+        tool.right(20)  
+        draw_tree(branch_length - 15)  
+        tool.left(40)  
+        draw_tree(branch_length - 15) 
+        tool.right(20) 
+        tool.backward(branch_length)  
+
+
+tool.left(90) 
+tool.penup()  
+tool.goto(0, -200) 
+tool.pendown() 
+draw_tree(100)
+
+
+for _ in range(5):
+    tool.penup
+    x = random.randint(-360, 360)
+    x = random.randint(150, 360)
+    tool.goto(x, y)
+    tool.pendown
+    for _ in range(2):
+        tool.forward(50)
+        tool.left(360/4)
+        tool.forward(50)
+        tool.left(360/4)
+    
+
 turtle.done()
 
 
